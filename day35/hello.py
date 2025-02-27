f0010ff95667d97122e070aaa57093695cd957d5
Alex <alexoh@wcoding.com>
2025-02-27 16:58:09 +0900

    Day 35

---
+++ day35/hello.py
@@ -0,0 +1,31 @@
+print("I'm Python. Nice to meet you!")
+print("Greetings", "and", "salutations", "to", "you!")
+
+# This is a comment!
+
+first_name = "Peter"
+last_name = "Parker"
+middle_name = "Paul"
+
+full_name = first_name + " " + middle_name + " " + last_name
+print(full_name)
+rev_name = last_name + " " + middle_name + " " + first_name
+print(rev_name)
+
+print("Hello world!"[6])
+print("Hello world!"[-6])
+
+total_len = len("Wow I am ready to program!")
+print(total_len)
+
+f_name = full_name[0:5]
+l_name = full_name[-6:]
+print(f_name)
+print(l_name)
+
+name = "Peter"
+age = 12
+job = "student"
+print(f"{name} is a {age} year old {job}.")
+
+
---
+++ day35/variablesmath.py
@@ -0,0 +1,13 @@
+import math
+
+square = 32.698 * 4
+print("Perimeter of square with side length 32.698:", square)
+
+rectangle = (22 * 2) + (99 * 2)
+print("Perimeter of rectangle with sides 22 and 99:", rectangle)
+
+# circle = (53.123 * 2) * 3.14
+circle = (53.123 * 2) * math.pi
+print("Circumference of circle:", circle)
+print("Circumference of circle:", 53.123 * 2 * math.pi)
+
